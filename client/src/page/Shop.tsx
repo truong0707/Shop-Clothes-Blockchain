56d8a5a3a3c1f8ae10580e192abfc89b72af3c1e
Truong Nguyen <truongit001fe@gmail.com>
2022-09-27 16:19:56 +0700

    set up project

--- client/src/page/Shop.tsx
+++ client/src/page/Shop.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import BodyShop from '../components/shop/BodyShop';
-import HeaderShop from '../components/shop/HeaderShop';
+
 
 export default function Shop() {
     return (
         <div style={{ marginTop: '64px' }}>
-            <HeaderShop />
-            <BodyShop />
+           Shop
         </div>
     )
 }
